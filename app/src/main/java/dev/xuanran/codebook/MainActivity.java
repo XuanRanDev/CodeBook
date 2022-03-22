48c56c77b6af769b81872dbfe0560ab3348953e4
XuanRanDev <3135419729@qq.com>
2022-03-22 12:30:21 +0800

    修复浮动按钮位置异常

--- app/src/main/java/dev/xuanran/codebook/MainActivity.java
+++ app/src/main/java/dev/xuanran/codebook/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -23,6 +24,7 @@ import androidx.appcompat.widget.PopupMenu;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
@@ -75,7 +77,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         setSupportActionBar(toolbar);
 
 
@@ -98,11 +99,18 @@ public class MainActivity extends AppCompatActivity {
         appBarLayout.addOnOffsetChangedListener(
                 new AppBarStatusChangeListener() {
                     @Override
-                    public void onStateChanged(AppBarLayout appBarLayout, State state) {
+                    public void onStateChanged(AppBarLayout appBarLayout, State state,int local) {
                         if (state == State.EXPANDED) {
                             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+                            floatingActionButton.animate().scaleX(1.0F).scaleY(1.0F).alpha(1.0F)
+                                    .setInterpolator(new FastOutSlowInInterpolator()).withLayer().setDuration(500)
+                                    .start();
                         } else if (state == State.COLLAPSED) {
                             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+                            floatingActionButton.animate().scaleX(0.0F).scaleY(0.0F).alpha(0.0F).setDuration(500).setInterpolator(new FastOutSlowInInterpolator()).withLayer();
+
+                        }else{
+                            Log.d("FloatDebug","当前位置：" + local);
                         }
                     }
                 });
@@ -136,8 +144,6 @@ public class MainActivity extends AppCompatActivity {
                         showAddPopupMenu(view);
                     }
                 });
-
-
             }
         });
     }
