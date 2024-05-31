5a86b9f38615ba55ba20d53b894aa24302e70a10
XuanRan <xuanrandev@qq.com>
2024-05-31 17:09:18 +0800

    feat:实现泡泡窗口

--- app/src/main/java/dev/xuanran/codebook/adapter/AccountAdapter.java
+++ app/src/main/java/dev/xuanran/codebook/adapter/AccountAdapter.java
@@ -1,14 +1,28 @@
 package dev.xuanran.codebook.adapter;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputLayout;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import dev.xuanran.codebook.R;
 import dev.xuanran.codebook.bean.AccountEntity;
@@ -33,8 +47,7 @@ public class AccountAdapter extends RecyclerView.Adapter<AccountViewHolder> {
         holder.tagText.setText(currentAccount.getTags());
 
         holder.viewButton.setOnClickListener(v -> {
-            // Handle view button click
-            Toast.makeText(v.getContext(), "View button clicked for " + currentAccount.getAppName(), Toast.LENGTH_SHORT).show();
+            showAccountDetailsDialog(v, currentAccount);
         });
     }
 
@@ -48,13 +61,61 @@ public class AccountAdapter extends RecyclerView.Adapter<AccountViewHolder> {
         notifyDataSetChanged();
     }
 
-    public AccountEntity getAccountAt(int position) {
-        return accounts.get(position);
+    private void showAccountDetailsDialog(View view, AccountEntity account) {
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(view.getContext(), R.style.ThemeOverlayAppMaterialAlertDialog);
+        LayoutInflater inflater = LayoutInflater.from(view.getContext());
+        View dialogView = inflater.inflate(R.layout.content_view_dialog, null);
+        builder.setView(dialogView);
+
+        AppCompatImageView more = dialogView.findViewById(R.id.content_view_dialog_more);
+        TextView title = dialogView.findViewById(R.id.content_view_dialog_title);
+        TextInputLayout accountIDInputLayout = dialogView.findViewById(R.id.content_view_dialog_accountID);
+        TextInputLayout passwordInputLayout = dialogView.findViewById(R.id.content_view_dialog_password);
+        Button copyAccountIDButton = dialogView.findViewById(R.id.content_view_dialog_copyAccountID);
+        Button copyPasswordButton = dialogView.findViewById(R.id.content_view_dialog_copyPassword);
+
+        title.setText(account.getAppName());
+        Objects.requireNonNull(accountIDInputLayout.getEditText()).setText(account.getAccount());
+        Objects.requireNonNull(passwordInputLayout.getEditText()).setText(account.getPassword());
+
+        copyAccountIDButton.setOnClickListener(v -> {
+            copyToClipboard(view.getContext(), "Account ID", account.getAccount());
+            Toast.makeText(view.getContext(), "Account ID copied", Toast.LENGTH_SHORT).show();
+        });
+
+        copyPasswordButton.setOnClickListener(v -> {
+            copyToClipboard(view.getContext(), "Password", account.getPassword());
+            Toast.makeText(view.getContext(), "Password copied", Toast.LENGTH_SHORT).show();
+        });
+
+        more.setOnClickListener(view13 -> showMoreMenu(view13, account));
+
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
-    public void filter(String text) {
-
+    private void copyToClipboard(Context context, String label, String text) {
+        ClipboardManager clipboard = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText(label, text);
+        clipboard.setPrimaryClip(clip);
     }
 
+    /**
+     * 显示卡片的更多菜单
+     *
+     * @param view 父View
+     */
+    private void showMoreMenu(View view, AccountEntity data) {
+        PopupMenu popup = new PopupMenu(view.getContext(), view);
+        popup.getMenuInflater().inflate(R.menu.dialog_content_menu, popup.getMenu());
+        popup.setOnMenuItemClickListener(item -> {
+            if (item.getItemId() == R.id.dialog_content_menu_delete) {
+                Snackbar.make(view, "数据删除后不可恢复，是否继续？", 10000).show();
+            }
+            return true;
+        });
+        popup.show();
+    }
 
 }
