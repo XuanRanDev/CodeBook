6c5dfb0f4706e2fe010470c85ca56a065221b987
XuanRan <xuanrandev@qq.com>
2025-02-19 12:55:31 +0800

    feat:添加了Toolbar

--- app/src/main/java/dev/xuanran/codebook/MainActivity.kt
+++ app/src/main/java/dev/xuanran/codebook/MainActivity.kt
@@ -1,9 +1,13 @@
 package dev.xuanran.codebook
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dev.xuanran.codebook.databinding.ActivityMainBinding
@@ -16,13 +20,62 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // 设置Toolbar
+        setSupportActionBar(binding.toolbar)
+
         // 获取 NavHostFragment
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
+        // 设置AppBarConfiguration
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.navigation_passwords, R.id.navigation_totp)
+        )
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+
         // 设置底部导航
         val navView: BottomNavigationView = binding.navView
         navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.top_app_bar, menu)
+        
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+        
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // TODO: 实现搜索逻辑
+                return true
+            }
+        })
+        
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.sort_name -> {
+                // TODO: 实现按名称排序
+                item.isChecked = true
+                true
+            }
+            R.id.sort_time -> {
+                // TODO: 实现按时间排序
+                item.isChecked = true
+                true
+            }
+            R.id.action_settings -> {
+                // TODO: 跳转到设置页面
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
